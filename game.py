f58c5bdc323984665bfa039d46760c2f217bc9d0
Ariston Zubiri <aristontheanalyst@gmail.com>
2025-04-01 23:39:15 +1300

    wrote comments to block out the program

---
+++ game.py
@@ -0,0 +1,16 @@
+# Welcome + Initialize empty variables
+
+# Ask name
+
+# Store name
+
+# Are there questions left?
+# - Needs a list of questions (Maybe a dictionary, or create a class?)
+
+# Randomly select unasked question until X questions have been asked, or if there are no more questions left.
+
+    # Player answers
+
+    # Cases for correct, incorrect and invalid answers
+
+# Announce game score, and closing message, ask the player if they want to play again
