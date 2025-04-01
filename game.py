29e382e81a37e1a90eea50d816aad07591632cbd
Ariston Zubiri <aristontheanalyst@gmail.com>
2025-04-02 00:38:42 +1300

    Wrote some variables, wrote a dictionary with questions, wrote an intro, started creating the main game loop

--- game.py
+++ game.py
@@ -1,8 +1,33 @@
-# Welcome + Initialize empty variables
+# Initialize empty variables
+name = ""
+score = 0
+question_number = 3
+questions = [
+    {"question" : "What is the national bird of New Zealand?",
+     "options" : ["Pukeko", "Hedgehog", "Raccoon", "Kiwi"],
+     "correct_answer" : "Kiwi"
+     },
+    {"question" : "What supermarket has recently changed their trade name to ""Woolworths"" ",
+     "options" : ["New World", "Pak N Save", "Barro", "Countdown"],
+     "correct_answer" : "Countdown"
+     },
+    {"question" : "What is New Zealand's premier rugby team?",
+     "options" : ["All Whites", "All Greys", "All Kiwis", "All Blacks"],
+     "correct_answer" : "All Blacks"
+     }
+]
 
-# Ask name
+# # Welcome
+# print("Welcome!")
 
-# Store name
+# # Ask name and Store name
+# name = input("What is your name?\n")
+# print("Welcome to the questionnaire game, "+name)
+# print("\nLets get started!")
+
+for i in range(question_number):
+    print(questions[i-1]["question"])
+    print("What questions")
 
 # Are there questions left?
 # - Needs a list of questions (Maybe a dictionary, or create a class?)
